a64034d22de0d8d99ccde64250e2b802e155905f
GuNShOtzZ <kevinloeding@outlook.de>
2024-03-07 02:22:08 +0100

    Create landing page with login- and registration button

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className=" h-screen flex justify-center items-center">
+      <div className=" bg-neutral-100 p-8 border-solid border-2 border-neutral-200 flex flex-col gap-2 text-center">
+        <h1 className=" font-bold text-xl">NextAuth.js with MongoDB</h1>
+        <Link href="/" className=" bg-blue-500 text-white rounded py-1">
+          Login
+        </Link>
+        <Link href="/" className=" bg-neutral-400 text-white rounded py-1">
+          Register
+        </Link>
+      </div>
+    </div>
   );
 }
